d581b9933809c24f2360316664820ed82196acb8
Rohit T Chauhan <101640000+rohit0420@users.noreply.github.com>
2023-07-13 18:26:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "John Doe",
+      enrollmentNo: "12345678",
+      age: 34
+    };
+  }
+
+  increaseAge = () => {
+    this.setState(prevState => ({ age: prevState.age + 1 }));
+  };
+
+  render() {
+    const { name, enrollmentNo, age } = this.state;
+
+    return (
+      <div>
+        <h1>Hello, my name is {name}</h1>
+        <p>I am {age} years old and my enrollment no is {enrollmentNo}</p>
+        <button onClick={this.increaseAge}>Increase Age</button>
+      </div>
+    );
+  }
+}
+
+export default App;
+
